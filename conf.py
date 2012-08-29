3c16e90b296828dd3cf3b04eb80d7c86337be3f8
Zach Oglesby <zach@oglesby.co>
2012-08-29 19:36:33 +0000

    Changed copywrite to say GoOSe Project contributors

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'GoOSe Project'
-copyright = u'CC-BY-SA, see AUTHORS for more detail'
+copyright = u'GoOSe Project Contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
